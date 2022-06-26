9a5a223d2195a7b9e92ad495b440de0d61c80236
SmolenskiyAV <JoJo_666@list.ru>
2022-06-26 21:50:50 +0700

    ver.0.3

--- my-app/src/components/ShopItemFunc.js
+++ my-app/src/components/ShopItemFunc.js
@@ -1,20 +1,36 @@
 /* eslint-disable array-callback-return */
-import React from "react";
+import React, { useState } from "react";
 // import PropTypes from "prop-types";
 // import UserModel from "../models/UserModel";
 
 export default function ShopItemFunc(props) {
   const { itemArray } = props;
-  // const testSrc = 'img/200.jpg';
-  function putItems(itemArray, index) {
+  const { onSelected } = props;
+  
+  console.log('onSelected = ', onSelected);
+  console.log('itemArray:', itemArray);
+  let filteredArray;
+  
+    if (onSelected !== "All") {
+      filteredArray = itemArray.filter((o) => o.category === onSelected);
+    } else filteredArray = itemArray;
+
+    
+    console.log("filter length =", filteredArray.length); // КОНТРОЛЬНАЯ ТОЧКА (длина результирующего отфильтрованного массива)
+    console.log("filtered array", filteredArray);
+    console.log("===========================================================");
+
+   
+
+  function putItems(itemArray, index) { // функция отрисовки элемента
     return (
       <img key={index} src={itemArray.img} alt={itemArray.category} />
     )
   };
-
-  return (
-    <div className="filterDiv cars">
-      {itemArray.map((itemArray, index) => putItems(itemArray, index))}
+  
+  return ( // отрисовка всего отфтльтрованного массива элементов
+    <div className="filterDiv">
+      {filteredArray.map((itemArray, index) => putItems(itemArray, index))}
     </div>
   );
 }
--- my-app/src/components/Toolbar.js
+++ my-app/src/components/Toolbar.js
@@ -1,43 +1,58 @@
 import React, { useState } from "react";
 import ShopItemFunc from "./ShopItemFunc";
 
-let valueSelected;
+let valueSelected = 'All'; // днфолтное значение фильтра при первоначальной загрузке страницы
 let btnSelected; // объект выбранной кнопки фильтра
 
 document.addEventListener('DOMContentLoaded', () => {
-  if ((btnSelected === undefined)) btnSelected = document.getElementById('myBtnContainer').querySelector('.active'); // объект выбранной кнопки изначально это кнопка "All"
-    console.log('initial btn', btnSelected);
+    if ((btnSelected === undefined)) btnSelected = document.getElementById('myBtnContainer').querySelector('.active'); // объект выбранной кнопки изначально это кнопка "All"
   }
 );
 
 export default function Toolbar(props) { // компонент кнопок фильтра
   const { itemsArray } = props;
 
-  const [selected, setSelected] = useState(btnSelected);
+  const [selected, setSelected] = useState('All');
+  const [pushed, setPushed] = useState(btnSelected);
   
-  const handleSelect = evt => {
+  const handlePush = evt => { // ОБРАБОТЛА НАЖАТИЯ КНОПОК ФИЛЬТРА
 
     btnSelected.classList.remove('active');
     
-    setSelected((prevSelected) => {
-      prevSelected = evt.target;
-      valueSelected = prevSelected.dataset.selected;
-      btnSelected = prevSelected;
+    setPushed((prevPushed) => { // обновление состояния нажатой кнопки фильтра (РАБОЧИЙ КОД)
+      prevPushed = evt.target;
+      valueSelected = prevPushed.dataset.selected; // получаем текущее значение фильтра по нажатой кнопке
+      btnSelected = prevPushed;
     });
 
     btnSelected.classList.add('active');
-};
+    console.log('valueSelected = ', valueSelected); // КОНТРОЛЬНАЯ ТОЧКА (выбранное значение фильтра)
+  };
+  
+  const handleSelected = valueSelected => { 
+    let result;
+
+    setSelected((prevSelected) => { // обновление состояния выбранного фильтра (НЕ РАБОТАЮЩИЙ КОД!) ...???...
+      prevSelected = valueSelected;
+      result = prevSelected;
+    });
+
+    return result; // попытка вывести значение выбранного фильтра valueSelected.
+  }
+
+  console.log('handleSelected() return: ', handleSelected()); // но функция handleSelected выдаёт  "undefined" (ПОЧЕМУ?)  и в результате в компонент ShopItemFunc ничего не передаётся. 
 
   return (
     // Control buttons
     <>
       <div id="myBtnContainer" className="myBtnContainer">
-        <button className="btn active" onClick={handleSelect} data-selected='all'> All</button>
-        <button className="btn " onClick={handleSelect} data-selected='websites'> Websites</button>
-        <button className="btn " onClick={handleSelect} data-selected='flayers'> Flayers</button>
-        <button className="btn " onClick={handleSelect} data-selected='business_cards'> Business Cards</button>
+        <button className="btn active" onClick={handlePush} data-selected='All'> All</button>
+        <button className="btn " onClick={handlePush} data-selected='Websites'> Websites</button>
+        <button className="btn " onClick={handlePush} data-selected='Flayers'> Flayers</button>
+        <button className="btn " onClick={handlePush} data-selected='Business Cards'> Business Cards</button>
       </div><div className="container">
-        <ShopItemFunc itemArray={itemsArray} />
+        <ShopItemFunc itemArray={itemsArray} onSelected={handleSelected} />
       </div></>
+      // попытка передать значение выбранного фильтра в дочерний компонент в стр.54 НЕУДАЧНА.. :/ Почему???
   );
 }
